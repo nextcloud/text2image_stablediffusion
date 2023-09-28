5c894bc3155254c269dffe6527930243f4e02eba
Marcel Klehr <mklehr@gmx.net>
2023-09-28 09:40:01 +0200

    Check in latest changes

--- src/stablediffusion.mjs
+++ src/stablediffusion.mjs
@@ -12,7 +12,8 @@ const __dirname = path.dirname(__filename);
 global.__dirname = __dirname
 
 const SEED = 1
-const NUM_INFERENCE_STEPS = 50
+const NUM_INFERENCE_STEPS = 25
+const THREADS = parseInt(process.env.STABLEDIFFUSION_THREADS || 6)
 
 async function main(inputText) {
 	if (inputText === '-') {
@@ -21,42 +22,50 @@ async function main(inputText) {
 
 	const textEncoder = await ort.InferenceSession.create(__dirname+'/../models/stable-diffusion-xl/text_encoder/model.onnx', {
 		executionMode: 'sequential',
-		executionProviders: ['cuda', 'cpu']
+		executionProviders: ['cuda', 'cpu'],
+		intraOpNumThreads: THREADS,
+		interOpNumThreads: THREADS
 	})
 
 	const uNet = await ort.InferenceSession.create(__dirname+'/../models/stable-diffusion-xl/unet/model.onnx', {
 		executionMode: 'sequential',
-		executionProviders: ['cuda', 'cpu']
+		executionProviders: ['cuda', 'cpu'],
+		intraOpNumThreads: THREADS,
+		interOpNumThreads: THREADS
 	})
 
 	const vaeDecoder = await ort.InferenceSession.create(__dirname+'/../models/stable-diffusion-xl/vae_decoder/model.onnx', {
 		executionMode: 'sequential',
-		executionProviders: ['cuda', 'cpu']
+		executionProviders: ['cuda', 'cpu'],
+		intraOpNumThreads: THREADS,
+		interOpNumThreads: THREADS
 	})
 
 	const textEncoder2 = await ort.InferenceSession.create(__dirname+'/../models/stable-diffusion-xl/text_encoder_2/model.onnx', {
 		executionMode: 'sequential',
-		executionProviders: ['cuda', 'cpu']
+		executionProviders: ['cuda', 'cpu'],
+		intraOpNumThreads: THREADS,
+		interOpNumThreads: THREADS
 	})
-
 
 	const tokenizerVocab = (await import('../models/stable-diffusion-xl/tokenizer/vocab.json', { assert: { type: "json" } })).default
 
 	const tokenizer2Vocab = (await import('../models/stable-diffusion-xl/tokenizer_2/vocab.json', { assert: { type: "json" } })).default
 
 	const textEncoderConfig = (await import('../models/stable-diffusion-xl/text_encoder/config.json', { assert: { type: "json" } })).default
-	const bos_token_id = textEncoderConfig.bos_token_id
-	const eos_token_id = textEncoderConfig.eos_token_id
 
-	let text_inputs = tokenize(inputText, tokenizerVocab).map(token => tokenizerVocab[token])
-	let inputTensor = new ort.Tensor('int32', Int32Array.from([bos_token_id, ...text_inputs, eos_token_id]), [1, text_inputs.length + 2])
+	let text_inputs = [49406, ...tokenize(inputText, tokenizerVocab).map(token => tokenizerVocab[token]).slice(0, 77-2), 49407]
+	text_inputs = text_inputs.concat(Array(77 - text_inputs.length).fill(49407))
+	console.log({text_inputs})
+	let inputTensor = new ort.Tensor('int32', Int32Array.from(text_inputs), [1, text_inputs.length])
 	const {last_hidden_state: lastHiddenState, pooler_output: pooledEmbeds} = await textEncoder.run({
 		input_ids: inputTensor
 	})
 
-	let text_inputs2 = tokenize(inputText, tokenizer2Vocab).map(token => tokenizer2Vocab[token])
-
-	let inputTensor2 = new ort.Tensor('int64', BigInt64Array.from([...[bos_token_id, ...text_inputs2, eos_token_id].map(num => BigInt(num))]), [1, text_inputs2.length + 2])
+	let text_inputs2 = [49406, ...tokenize(inputText, tokenizer2Vocab).map(token => tokenizer2Vocab[token]).slice(0, 77-2), 49407]
+	text_inputs2 = text_inputs2.concat(Array(77 - text_inputs2.length).fill(49407))
+	console.log({text_inputs2})
+	let inputTensor2 = new ort.Tensor('int64', BigInt64Array.from(text_inputs2.map(num => BigInt(num))), [1, text_inputs2.length])
 	const {text_embeds: textEmbeds, last_hidden_state: lastHiddenState2 } = await textEncoder2.run({
 		input_ids: inputTensor2
 	})
@@ -66,6 +75,12 @@ async function main(inputText) {
 		tf.tensor(lastHiddenState.data, lastHiddenState.dims),
 		tf.tensor(lastHiddenState2.data, lastHiddenState2.dims)
 	], -1)
+
+	console.log({
+		lastHiddenState,
+		lastHiddenState2,
+		textEmbeds,
+	})
 
 
 	const vaeEncoderConfig = (await import('../models/stable-diffusion-xl/vae_encoder/config.json', { assert: { type: "json" } })).default
@@ -85,9 +100,7 @@ async function main(inputText) {
 	// set_timesteps
 	const step_ratio = Math.round(schedulerConfig.num_train_timesteps / NUM_INFERENCE_STEPS)
 	// creates integer timesteps by multiplying by ratio
-	// casting to int to avoid issues when num_inference_step is power of 3
-	let timesteps = tf.tidy(() => tf.range(0, NUM_INFERENCE_STEPS).mul(tf.scalar(step_ratio)).round())
-	timesteps.add(schedulerConfig.steps_offset)
+	let timesteps = tf.tidy(() => tf.range(0, NUM_INFERENCE_STEPS).mul(tf.scalar(step_ratio)).round().add(schedulerConfig.steps_offset).reverse())
 	timesteps = await timesteps.array()
 
 	const sigmas = tf.tidy(() => {
@@ -97,14 +110,16 @@ async function main(inputText) {
 		return tf.scalar(1.0).sub(alphas_cumprod).div(alphas_cumprod).pow(0.5)
 	})
 
-	const interp_sigmas = await interp(timesteps, tf.range(0, schedulerConfig.num_train_timesteps), sigmas)
+	console.log({sigmas: await sigmas.data(), sigmars: await sigmas.reverse().data()})
+	console.log({timesteps})
+
+	let interp_sigmas = await interp(timesteps, tf.range(0, schedulerConfig.num_train_timesteps), sigmas)
+	interp_sigmas = tf.concat([interp_sigmas, [0.0]])
 
 	console.log({interp_sigmas: await interp_sigmas.data()})
-	const concat_sigmas = tf.concat([interp_sigmas, [0.0]])//.div(interp_sigmas.max())
-
 	console.log({latents: await latents.data()})
 
-	const init_noise_sigma = tf.tidy(() => concat_sigmas.max().pow(2).add(tf.scalar(1)).pow(0.5))
+	const init_noise_sigma = tf.tidy(() => interp_sigmas.max().pow(2).add(tf.scalar(1)).pow(0.5))
 	console.log({init_noise_sigma: await init_noise_sigma.data()})
 	latents = latents.mul(init_noise_sigma)
 
@@ -116,7 +131,7 @@ async function main(inputText) {
 	const allPromptEmbedsTensor = new ort.Tensor('float32', await allPromptEmbeds.data(), allPromptEmbeds.shape)
 	let i = 0
 	for (let t of timesteps) {
-		const sigmasArray = await concat_sigmas.array()
+		const sigmasArray = await interp_sigmas.array()
 		const sigma = sigmasArray[i]
 
 		const sample = tf.tidy(() => latents.div(tf.scalar(sigma).pow(2).add(1).pow(0.5)))
@@ -133,7 +148,7 @@ async function main(inputText) {
 		})
 
 		console.log({
-			i, latents: await latents.data(), noisePred: noisePredTensor.data
+			i, latents: {shape: latents.shape, data: await latents.data()}, noisePred: noisePredTensor
 		})
 
 		latents = tf.tidy(() => {
@@ -141,7 +156,7 @@ async function main(inputText) {
 			const noisePred = tf.tensor(noisePredTensor.data, noisePredTensor.dims, 'float32')
 			const pred_original_sample = latents.sub(sigmaTf.mul(noisePred))
 			const derivative = latents.sub(pred_original_sample).div(sigmaTf)
-			const dt = Math.abs(sigmasArray[i + 1] - sigma)
+			const dt = sigmasArray[i + 1] - sigma
 			derivative.data().then(derivative => console.log({dt, derivative}))
 			return latents.add(derivative.mul(tf.scalar(dt)))
 		})
@@ -169,9 +184,8 @@ await main(
 	process.argv[2]
 )
 
-
 function tokenize(text, vocab) {
-	const words = text.replace(/\s/, ' ').replace('\n', ' ').split(' ').filter(word => word !== '')
+	const words = text.toLowerCase().replace(/\s/, ' ').replace('\n', ' ').split(' ').filter(word => word !== '')
 	const tokens = []
 	for (let i=0; i<words.length; i++) {
 		let currentToken = ''
